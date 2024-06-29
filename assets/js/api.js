391f3eeb6dec754454d9e831bd3ebf475095efad
Dj3y <dkyamilova@gmail.com>
2024-06-29 16:07:38 +0200

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,14 +1,15 @@
 import {displayResult} from "./displayResult.js";
 
-// // fonction pour récuper l'api 
+// fonction pour récuper l'api 
 export function recupApi(){
     const cityName = document.getElementById("input-city").value;
     const apiKey = "314b05dedcf168f888186a0791c643ce";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
     // if (!cityName) {
     //     alert('Please enter a name.');
     //     return;
     // } https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric 
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`)
+    fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
         displayResult(data);
@@ -21,22 +22,17 @@ export function recupApi(){
 }
 
 const divResultat = document.querySelector('#display-result');
-console.log(divResultat);
 // ajout d'un évenement pour le bouton
 const btnSearch = document.getElementById('btn-search');
 btnSearch.addEventListener('click', ()=> {
     recupApi();
-    displayResult();
     divResultat.style.display = "block";
-    // tempJourSuivants();
 });
 // ajout d'un évenement pour la touche 'enter'
 document.getElementById('input-city').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
       recupApi();
-      displayResult(); 
       divResultat.style.display = "block";
-    //   tempJourSuivants();
     }
 });
 
